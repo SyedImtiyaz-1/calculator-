976cb3511c58e88f57161d3088056c5771061fa1
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-03-24 00:10:14 +0530

    updated js

--- script.js
+++ script.js
@@ -2,22 +2,18 @@
 let string = "";
 let buttons = document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
+  button.addEventListener('click', (s)=>{
+    if(s.target.innerHTML == '='){
       string = eval(string);
       document.querySelector('input').value = string;
     }
-    else if(e.target.innerHTML == 'C'){
+    else if(s.target.innerHTML == 'C'){
         string = "";
         document.querySelector('input').value = string;
       }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
     else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
+    console.log(s.target)
+    string = string + s.target.innerHTML;
     document.querySelector('input').value = string;
       }
   })
