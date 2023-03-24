8846188ac0aa48720c7089987cddb23fcfd4b721
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-03-24 16:04:41 +0530

    added backspace button

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 let string = "";
-let buttons = document.querySelectorAll('.button');
+let buttons = document.querySelectorAll(".button");
 
 Array.from(buttons).forEach((button) => {
-  button.addEventListener('click', (s) => {
+  button.addEventListener("click", (s) => {
     try {
-      if (s.target.innerHTML == '=') {
+      if (s.target.innerHTML == "=") {
         string = eval(string);
-        document.querySelector('input').value = string;
-      } else if (s.target.innerHTML == 'C') {
+        document.querySelector("input").value = string;
+      } else if (s.target.innerHTML == "C") {
         string = "";
-        document.querySelector('input').value = string;
+        document.querySelector("input").value = string;
       } else {
         console.log(s.target);
         let lastChar = string.charAt(string.length - 1);
         let newChar = s.target.innerHTML;
-        if ((lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/' || lastChar == '%') && (newChar == '+' || newChar == '*' || newChar == '/' || newChar == '%')) {
-          throw new Error('Invalid input');
+        if (
+          (lastChar == "+" ||lastChar == "-" ||lastChar == "*" ||lastChar == "/" ||lastChar == "%") && (newChar == "+" || newChar == "*" || newChar == "/" || newChar == "%")
+        ) {
+          throw new Error("Invalid input");
         }
         string = string + newChar;
-        document.querySelector('input').value = string;
+        document.querySelector("input").value = string;
       }
     } catch (error) {
       console.error(error);
       string = "";
-      document.querySelector('input').value = 'Invalid input';
+      document.querySelector("input").value = "Invalid input";
     }
-
   });
+});
+backspace.addEventListener("click", () => {
+  string = string.slice(0, -1);
+  document.querySelector("input").value = string;
 });
