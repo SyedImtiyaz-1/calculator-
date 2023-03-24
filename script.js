5ddabcd195df4312d5f2ddda1a597954cb2c69d5
Syed Imtiyaz Ali <syedimtiyazali141@gmail.com>
2023-03-24 12:38:15 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-// Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.button');
+
 Array.from(buttons).forEach((button)=>{
   button.addEventListener('click', (s)=>{
-    if(s.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(s.target.innerHTML == 'C'){
+    try {
+      if(s.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value = string;
+      } else if(s.target.innerHTML == 'C'){
         string = "";
         document.querySelector('input').value = string;
+      } else{ 
+        console.log(s.target);
+        let lastChar = string.charAt(string.length-1);
+        let newChar = s.target.innerHTML;
+        if ((lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/' || lastChar == '%' ) && (newChar == '+' || newChar == '*' || newChar == '/' || newChar == '%')) {
+          throw new Error('Invalid input');
+        }
+        string = string + newChar;
+        document.querySelector('input').value = string;
       }
-    else{ 
-    console.log(s.target)
-    string = string + s.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+    } catch (error) {
+      console.error(error);
+      string = "";
+      document.querySelector('input').value = 'Invalid input';
+    }
+  });
+});
